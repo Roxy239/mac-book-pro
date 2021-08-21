92fef09b6ae9d968bf0b5d5f44c494d9dd74f0a5
Roxy239 <rahatara239@gmail.com>
2021-08-21 20:55:43 +0600

    second commit

--- mac-book-pro-main/js/app.js
+++ mac-book-pro-main/js/app.js
@@ -1,1 +1,48 @@
-console.log('i am connected');
+function calculatePrice(specificId, price) {
+
+
+    const TableInput = document.getElementById(specificId + '-cost');
+    // const Tablevalue = TableInput.innerText;
+    TableInput.innerText = price;
+
+}
+
+
+
+
+document.getElementById('8gb-memory').addEventListener('click', function () {
+
+
+    calculatePrice('memory', 0);
+})
+
+document.getElementById('16gb-memory').addEventListener('click', function () {
+
+    calculatePrice('memory', 180);
+
+})
+document.getElementById('256gb-storage').addEventListener('click', function () {
+
+    calculatePrice('storage', 0);
+
+})
+document.getElementById('512gb-storage').addEventListener('click', function () {
+
+    calculatePrice('storage', 100);
+
+})
+document.getElementById('1tb-storage').addEventListener('click', function () {
+
+    calculatePrice('storage', 180);
+
+})
+document.getElementById('regular-delivery').addEventListener('click', function () {
+
+    calculatePrice('delivery', 0);
+
+})
+document.getElementById('urgent-delivery').addEventListener('click', function () {
+
+    calculatePrice('delivery', 20);
+
+})
